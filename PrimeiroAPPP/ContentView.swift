02733fad09b4acd04ffc7539b68309f0133ebc47
Thaynara-Andrade <thaynara.silva.andrade07@aluno.ifce.edu.br>
2022-05-11 09:15:55 -0300

    Initial Commit

---
+++ PrimeiroAPPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PrimeiroAPPP
+//
+//  Created by Thaynara da Silva Andrade on 11/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PrimeiroAPPP/PrimeiroAPPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeiroAPPPApp.swift
+//  PrimeiroAPPP
+//
+//  Created by Thaynara da Silva Andrade on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PrimeiroAPPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
